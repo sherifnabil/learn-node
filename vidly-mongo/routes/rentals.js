8802177473435cd2dac71e1860ba2ae9a3975918
Sherif <sherifnabil@github.com>
2021-11-16 13:20:49 +0200

    2 phase commit with Fawn lib

--- vidly-mongo/routes/rentals.js
+++ vidly-mongo/routes/rentals.js
@@ -1,9 +1,12 @@
 const {Rental, validate} = require('../models/rental');
-const {Movie} = require('../models/movie');
 const {Customer} = require('../models/customer');
+const { Movie } = require('../models/movies');
 const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
+const Fawn = require('fawn')
+
+Fawn.init('mongodb://localhost/playground');
 
 router.get('/', async (req, res) => {
   const rentals = await Rental.find().sort('-dateOut');
@@ -34,12 +37,19 @@ router.post('/', async (req, res) => {
       dailyRentalRate: movie.dailyRentalRate
     }
   });
-  rental = await rental.save();
 
-  movie.numberInStock--;
-  movie.save();
-
-  res.send(rental);
+  try {
+    new Fawn.Task()
+      .save('rentals', rental)
+      .update('movies', { _id: movie._id }, {
+        $inc: { numberInStock: -1 }
+      })
+      .run();
+    res.send(rental);
+  }
+  catch(ex) {
+    res.status(500).send('Somthing Failed.')
+  }
 });
 
 router.get('/:id', async (req, res) => {
