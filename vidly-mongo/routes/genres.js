5cba1556357c832154675234ee67c16697355c88
Sherif <sherifnabil@github.com>
2021-11-17 19:35:54 +0200

    admin middleware for deletion operations

--- vidly-mongo/routes/genres.js
+++ vidly-mongo/routes/genres.js
@@ -1,6 +1,7 @@
+const auth = require('../middleware/auth')
+const admin = require('../middleware/admin')
 const express = require('express');
 const router = express.Router()
-const auth = require('../middleware/auth')
 const { Genre, validate } = require('../models/genre');
 // router.get('/', (req, res) => {  // 1
 //   const genres = Genre.find();
@@ -35,7 +36,7 @@ router.put('/:id', auth, async (req, res) => {
   res.send(genre);
 });
 
-router.delete('/:id', auth, async (req, res) => {
+router.delete('/:id', [auth, admin], async (req, res) => {
   const genre = await Genre.findByIdAndRemove(req.params.id)
 
   if (!genre) return res.status(404).send('The genre with the given ID was not found.');
