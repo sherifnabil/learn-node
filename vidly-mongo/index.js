9061952e599345f324f8b13b303952d99d16bfc1
Sherif <sherifnabil@github.com>
2021-11-21 15:50:45 +0200

    shall use winston in logging

--- vidly-mongo/index.js
+++ vidly-mongo/index.js
@@ -8,4 +8,4 @@ require('./startup/config')();
 require('./startup/validation')();
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Listening on port ${port}...`));
+app.listen(port, () => console.log(`Listening on port ${port}...`)); // should be done with winston
