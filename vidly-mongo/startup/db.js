42660ebf3bc8d358f2566ed64fce487f2d066e99
Sherif <sherifnabil@github.com>
2021-11-21 15:26:54 +0200

    let the error handler handle uncaught exception for DB connecting

--- vidly-mongo/startup/db.js
+++ vidly-mongo/startup/db.js
@@ -1,10 +1,6 @@
 const mongoose = require('mongoose');
 
-
 module.exports = function() {
   mongoose.connect('mongodb://localhost/playground')
   .then(() => console.log('Connected....'))
-  .catch((e) => console.log('Error ', e))
-
-
 }
