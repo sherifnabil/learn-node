d9100d4457e0a16ec0a9e340d987b72199313ad2
Sherif <sherifnabil@github.com>
2021-11-21 15:40:11 +0200

    moving logging errors from index to a seperate file

--- vidly-mongo/index.js
+++ vidly-mongo/index.js
@@ -1,37 +1,17 @@
-const winston = require('winston');
-require('express-async-errors');
 const config = require('config');
 const Joi = require('joi');
 Joi.objectId = require('joi-objectid')(Joi);
 const express = require('express');
 const app = express();
+require('./startup/logging')(); // put first just incase in loading other modules would log that error and terminate the process
 require('./startup/routes')(app);
 require('./startup/db')();
-
-process.on('uncaughtException', (ex)  =>  {
-  console.log('WE GOT AN UNCAUGHT EXCEPTION!!!', ex);
-  // winston.add(new winston.transports.File({ filename: 'error.log', level: 'error', message: "WE GOT AN UNCAUGHT EXCEPTION" }))
-
-});
 
 // when export env variable run project name _ your variable as next export vidly_jwtPrivateKey=mySecureKey
 if (!config.get('jwtPrivateKey')) {
   console.error('FATAL ERROR: jwtPrivateKey was not defined.');
   process.exit(1);
 }
-
-// throw new Error('Something Failed during startup.');
-
-// const p = Promise.reject(new Error('Something Failed Miserably!'));
-// p.then(r => console.log('Done'));
-
-process.on('unhandledRejection', (ex)  =>  {
-  winston.exceptions.handle(new winston.transports.File({ filename: 'unhandledExceptions.log'}))
-  // process.exit(1);
-  throw ex;
-});
-
-
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
---
+++ vidly-mongo/startup/logging.js
@@ -0,0 +1,20 @@
+const winston = require('winston');
+require('winston-mongodb');
+require('express-async-errors');
+
+module.exports = function() {
+  // throw new Error('Something Failed during startup.');
+  // const p = Promise.reject(new Error('Something Failed Miserably!'));
+  // p.then(r => console.log('Done'));
+  process.on('unhandledRejection', (ex)  =>  {
+    winston.exceptions.handle(new winston.transports.File({ filename: 'unhandledExceptions.log'}))
+    // process.exit(1);
+    throw ex;
+  });
+
+  process.on('uncaughtException', (ex)  =>  {
+    console.log('WE GOT AN UNCAUGHT EXCEPTION!!!', ex);
+    winston.add(new winston.transports.File, { filename: 'error.log'})
+    winston.add(new winston.transports.MongoDB, { db: 'mongodb://localhost/playground', level: 'info'})
+  });
+}
