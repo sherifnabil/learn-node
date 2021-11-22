3cc674a6b01dfaf0e43542e76e7e9a9bc10630c0
Sherif <sherifnabil@github.com>
2021-11-22 05:56:14 +0200

    grouping tests

--- testing-demo/tests/lib.test.js
+++ testing-demo/tests/lib.test.js
@@ -1,16 +1,18 @@
 const lib = require('../lib');
 
-test('absloute - should return positive number if the input is positive', () => {
-  const result = lib.absolute(1);
-  expect(result).toBe(1);
-});
+describe('absloute', () => {
+  it('should return positive number if the input is positive', () => {
+    const result = lib.absolute(1);
+    expect(result).toBe(1);
+  });
 
-test('absloute - should return positive number if the input is negative', () => {
-  const result = lib.absolute(-1);
-  expect(result).toBe(1);
-});
+  it('should return positive number if the input is negative', () => {
+    const result = lib.absolute(-1);
+    expect(result).toBe(1);
+  });
 
-test('absloute - should return 0 if the input is 0', () => {
-  const result = lib.absolute(0);
-  expect(result).toBe(0);
+  it('should return 0 if the input is 0', () => {
+    const result = lib.absolute(0);
+    expect(result).toBe(0);
+  });
 });
