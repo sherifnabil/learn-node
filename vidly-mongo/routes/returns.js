5e36183c579e0eaa6a034bf1a9f34b4ead455830
Sherif <sherifnabil@github.com>
2021-12-19 12:43:32 +0200

    adding returns routes

---
+++ vidly-mongo/routes/returns.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const Joi = require('Joi');
+const auth = require('../middleware/auth');
+const validate = require('../middleware/validate');
+const router = express.Router()
+const {Rental} = require('../models/rental');
+const {Movie} = require('../models/movie');
+const moment = require('moment');
+
+// const validate = (validator) => {
+//   return (req, res, next) => {
+//     const { error } = validator(req.body);
+//     if (error) return res.status(400).send(error.details[0].message);
+//   }
+// }
+
+router.post('/', [auth, validate(validateReturn)], async (req, res) => {
+
+
+  const rental = await Rental.findOne({
+    'customer._id': req.body.customerId,
+    'movie._id': req.body.movieId,
+  });
+
+  if(!rental) return res.status(404).send('not found');
+
+  if(rental.dateReturned) return res.status(400).send('Return alrady processed');
+  rental.dateReturned = new Date();
+
+  const rentalDays = moment().diff(rental.dateOut, 'days');
+  rental.rentalFee = rentalDays * rental.movie.dailyRentalRate;
+  await rental.save();
+
+  await Movie.updateOne({ _id: rental.movie._id }, {
+    $inc: {
+      numberInStock: 1
+    }
+  });
+
+  return res.status(200).send(rental);
+});
+
+function validateReturn(req) {
+  const schema = Joi.object({
+    customerId: Joi .objectId().required(),
+    movieId: Joi .objectId().required(),
+  });
+  return schema.validate(req);
+}
+
+module.exports = router;
--- vidly-mongo/startup/routes.js
+++ vidly-mongo/startup/routes.js
@@ -1,6 +1,7 @@
 
 const express = require('express');
 const genres = require('../routes/genres')
+const returns = require('../routes/returns')
 const customers = require('../routes/customers')
 const movies = require('../routes/movies')
 const rentals = require('../routes/rentals');
@@ -11,6 +12,7 @@ const error = require('../middleware/error');
 module.exports = function(app) {
   app.use(express.json());
   app.use('/api/genres', genres);
+  app.use('/api/returns', returns);
   app.use('/api/customers', customers);
   app.use('/api/movies', movies);
   app.use('/api/rentals', rentals);
